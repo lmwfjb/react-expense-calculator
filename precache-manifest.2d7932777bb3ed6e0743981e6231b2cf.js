2e83c7b471a12ed2a54638c8f872fb8f80105519
lmw <lmwfjb@gmail.com>
2024-07-07 19:39:16 +0800

    Updates

---
+++ precache-manifest.2d7932777bb3ed6e0743981e6231b2cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bb73857b8432f846d98dc36cd17b28",
+    "url": "/react-expense-calculator/index.html"
+  },
+  {
+    "revision": "72c2f7fd937f79e63495",
+    "url": "/react-expense-calculator/static/css/2.2a42abf6.chunk.css"
+  },
+  {
+    "revision": "f7ece614558dc8fcb161",
+    "url": "/react-expense-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "72c2f7fd937f79e63495",
+    "url": "/react-expense-calculator/static/js/2.e88095bb.chunk.js"
+  },
+  {
+    "revision": "d0775e084c9afc672e19",
+    "url": "/react-expense-calculator/static/js/3.c114a520.chunk.js"
+  },
+  {
+    "revision": "f7ece614558dc8fcb161",
+    "url": "/react-expense-calculator/static/js/main.d346005c.chunk.js"
+  },
+  {
+    "revision": "2a4786ef7fec47da1bee",
+    "url": "/react-expense-calculator/static/js/runtime~main.9386f479.js"
+  }
+]);
